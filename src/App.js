cfe98b65b6e2f64bc51ede344afd364c6d1cb3e5
sarika296 <sarikadewangan94@gmail.com>
2020-05-02 10:41:24 +0530

    video 12 and 13.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import Projects from './Projects';
 
 class App extends Component {
     constructor()
@@ -32,6 +33,8 @@ class App extends Component {
 
                 )
         }
+        <hr/>
+        <Projects />
             </div>
         )
     }
