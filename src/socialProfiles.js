8a83189c52b3ff0542a0d084fbd83f8a039fed8c
sarika296 <sarikadewangan94@gmail.com>
2020-05-02 10:59:17 +0530

    video 14.

---
+++ src/socialProfiles.js
@@ -0,0 +1,28 @@
+import emailIcon from '..assets/email_icon.png';
+import githubIcon from '..assets/github_icon.png';
+import linkedinIcon from '..assets/linkkedin_icon.png';
+import twitterIcon from '..assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        link:'mailto:surualone@gmail.com',
+        image:emailIcon
+    },
+    {
+        id: 2,
+        link:'mailto://github.com/sarika296',
+        image:githubIcon
+    },
+    {
+        id: 3,
+        link:'mailto: www.linkedin.com/in/sarika-dewangan',
+        image:linkedinIcon
+    },
+    {
+        id: 4,
+        link:'mailto:surualone@gmail.com',
+        image:twitterIcon
+    }
+];
+export default SOCIAL_PROFILES;
